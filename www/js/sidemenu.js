c1e3d27e22a52c905a88257a21c1ae57a615b1f6
joroni <jraymund.niconi@gmail.com>
2016-04-18 17:53:26 +0800

    side menu

--- www/js/sidemenu.js
+++ www/js/sidemenu.js
@@ -38,8 +38,8 @@ $(document).ready(function(){
 				
 				
 				
-				//var iconPath = config.fileSystemRootFolder + '/' + config.localImageFolderPath + '/' + code.toLowerCase() + ".png?" + Math.random();
-				var iconPath = config.localImageFolderPath + '/' + code.toLowerCase() + ".png?" + Math.random();						
+				var iconPath = config.fileSystemRootFolder + '/' + config.localImageFolderPath + '/' + code.toLowerCase() + ".png?" + Math.random();
+				//var iconPath = config.localImageFolderPath + '/' + code.toLowerCase() + ".png?" + Math.random();						
 				if (!localFileExists(iconPath)) {
 					iconPath = config.defaultIconPath;
 				}
